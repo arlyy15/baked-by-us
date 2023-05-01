ed3634a9dbb574ace37e25c7ac1e65e433075737
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2023-05-01 10:34:14 -0400

    Update init.js

--- src/db/seeds/init.js
+++ src/db/seeds/init.js
@@ -1,3 +1,9 @@
+/* This seed file uses the existing `User` model to 
+create new users with hashed passwords. To seed your 
+database with other User data, update your model's `create` 
+function to accept other input fields first before updating
+this file.
+*/
 const User = require('../models/user');
 /**
  * @param { import("knex").Knex } knex
